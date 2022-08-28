e8c520befdbe8edd6eddb89c5692617b12ef7f0e
com0rade1 <protasovnekit@gmail.com>
2022-08-28 20:26:57 +0300

    app uploaded

---
+++ Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Step = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Run = new System.Windows.Forms.Button();
+            this.ScaleBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GameField = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ScaleBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(615, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 23);
+            this.button3.TabIndex = 29;
+            this.button3.Text = "Задать правило";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(615, 185);
+            this.maskedTextBox1.Mask = "0000000000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 28;
+            this.maskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(659, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "01000000000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(612, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Default";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(626, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Задать правило";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(564, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 38);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "Сгенерировать автомат";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(615, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 51);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Продолжить до конца поля";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Step
+            // 
+            this.Step.Location = new System.Drawing.Point(615, 57);
+            this.Step.Name = "Step";
+            this.Step.Size = new System.Drawing.Size(112, 38);
+            this.Step.TabIndex = 22;
+            this.Step.Text = "Сделать 1 шаг";
+            this.Step.UseVisualStyleBackColor = true;
+            this.Step.Click += new System.EventHandler(this.Step_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(470, 23);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 20);
+            this.textBox1.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(314, 36);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 15);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Количество указано тут";
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(329, 12);
+            this.Run.Margin = new System.Windows.Forms.Padding(2);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(121, 24);
+            this.Run.TabIndex = 19;
+            this.Run.Text = "Задать масштаб";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // ScaleBar
+            // 
+            this.ScaleBar.LargeChange = 1;
+            this.ScaleBar.Location = new System.Drawing.Point(72, 6);
+            this.ScaleBar.Margin = new System.Windows.Forms.Padding(2);
+            this.ScaleBar.Maximum = 5;
+            this.ScaleBar.Name = "ScaleBar";
+            this.ScaleBar.Size = new System.Drawing.Size(238, 45);
+            this.ScaleBar.TabIndex = 18;
+            this.ScaleBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Масштаб: ";
+            // 
+            // GameField
+            // 
+            this.GameField.BackColor = System.Drawing.Color.White;
+            this.GameField.Location = new System.Drawing.Point(10, 55);
+            this.GameField.Margin = new System.Windows.Forms.Padding(2);
+            this.GameField.Name = "GameField";
+            this.GameField.Size = new System.Drawing.Size(600, 600);
+            this.GameField.TabIndex = 16;
+            this.GameField.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(616, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 13);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "Состояние 0 - красный";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(612, 254);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 13);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "1 - зелеленый";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(616, 267);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(109, 13);
+            this.label8.TabIndex = 32;
+            this.label8.Text = "Состояние 2 - синий";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(615, 284);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 52);
+            this.button4.TabIndex = 33;
+            this.button4.Text = "Генерация случайного правила";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 661);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Step);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.ScaleBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GameField);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ScaleBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameField)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Step;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.TrackBar ScaleBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox GameField;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        int Scale;
+        Random rnd = new Random();
+        int[] InputData = new int[301];
+        int[] TempData = new int[301];
+        int[] BordPixNum = new int[301];
+        int Count;
+        int PrintStr;
+        string LifeRule = "01000000000";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            this.Scale = 2 * (ScaleBar.Value + 1);
+            Graphics g = GameField.CreateGraphics();
+            g.Clear(Color.White);
+            this.Count = (int)(GameField.Width / this.Scale);
+            textBox1.Text = Convert.ToString(this.Count);
+
+        }
+
+        private void Step_Click(object sender, EventArgs e)
+        {
+            Graphics g = GameField.CreateGraphics();
+            int ALive = 0;
+            this.PrintStr++;
+            if (PrintStr < Count)
+            {
+                for (int i = 0; i < this.Count; i++)
+                {
+                    ALive = 0;
+                    if (i == 0)
+                    {
+                        ALive = InputData[Count - 2] + InputData[Count - 1] + InputData[0] + InputData[1] + InputData[2];
+                    }
+                    if (i == 1)
+                    {
+                        ALive = InputData[Count - 1] + InputData[0] + InputData[1] + InputData[2] + InputData[3];
+                    }
+                    if (i == Count - 1)
+                    {
+                        ALive = InputData[Count - 3] + InputData[Count - 2] + InputData[Count - 1] + InputData[0] + InputData[1];
+                    }
+                    if (i == Count - 2)
+                    {
+                        ALive = InputData[Count - 4] + InputData[Count - 3] + InputData[Count - 2] + InputData[Count - 1] + InputData[0];
+                    }
+                    if (i != 0 && i != 1 && i != Count - 2 && i != Count - 1)
+                    {
+                        int k = -2;
+                        while ((k - 3) != 0)
+                        {
+                            ALive += InputData[i + k];
+                            k++;
+                        }
+                    }
+                    if (Convert.ToInt32(LifeRule[ALive]) == '0')
+                    {
+                        TempData[i] = 0;
+                    }
+                    else if (Convert.ToInt32(LifeRule[ALive]) == '1')
+                    {
+                        TempData[i] = 1;
+                    }
+                    else if (Convert.ToInt32(LifeRule[ALive]) == '2')
+                    {
+                        TempData[i] = 2;
+                    }
+                }
+                for (int i = 0; i < Count; i++)
+                {
+                    if (TempData[i] == 0)
+                    {
+                        g.FillRectangle(Brushes.Red, BordPixNum[i], BordPixNum[PrintStr], this.Scale, this.Scale);
+                    }
+                    else if (TempData[i] == 1)
+                    {
+                        g.FillRectangle(Brushes.Green, BordPixNum[i], BordPixNum[PrintStr], this.Scale, this.Scale);
+                    }
+                    else if (TempData[i] == 2)
+                    {
+                        g.FillRectangle(Brushes.Blue, BordPixNum[i], BordPixNum[PrintStr], this.Scale, this.Scale);
+                    }
+                }
+                for (int i = 0; i < Count; i++)
+                {
+                    InputData[i] = TempData[i];
+                }
+            }
+            else
+            {
+                g.Clear(Color.White);
+                PrintStr = -1;
+                this.Step_Click(sender, e);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            while (PrintStr < Count - 1)
+            {
+                this.Step_Click(sender, e);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Graphics g = GameField.CreateGraphics();
+            g.Clear(Color.White);
+            int k = 0;
+            for (int i = 0; i <= GameField.Width; i += Scale)
+            {
+                this.BordPixNum[k] = i;
+                k++;
+            }
+            for (int i = 0; i < Count; i++)
+            {
+                InputData[i] = rnd.Next(0, 3);
+            }
+            for (int i = 0; i < Count; i++)
+            {
+                if (InputData[i] == 0)
+                {
+                    g.FillRectangle(Brushes.Red, BordPixNum[i], 0, this.Scale, this.Scale);
+                }
+                else if (InputData[i] == 1)
+                {
+                    g.FillRectangle(Brushes.Green, BordPixNum[i], 0, this.Scale, this.Scale);
+                }
+                else if (InputData[i] == 2)
+                {
+                    g.FillRectangle(Brushes.Blue, BordPixNum[i], 0, this.Scale, this.Scale);
+                }
+            }
+            this.PrintStr = 0;
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (maskedTextBox1.Text != "")
+            {
+                this.LifeRule = maskedTextBox1.Text;
+                while (LifeRule.Length < 11)
+                {
+                    this.LifeRule = this.LifeRule + "0";
+                }
+            }
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            string Rule = "";
+            maskedTextBox1.Text = "";
+            for (int i = 0; i<=10;i++)
+            {
+                Rule += rnd.Next(0, 3);
+            }
+            this.LifeRule = Rule;
+            maskedTextBox1.Text = Rule;
+        }
+    }
+}
+
